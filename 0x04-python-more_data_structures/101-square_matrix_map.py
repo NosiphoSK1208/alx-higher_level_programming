3f5215f092059c7cb79b08ad471f37a8bb4dae26
NosiphoSK1208 <Simphiwe95.k@gmail.com>
2023-05-17 20:06:48 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-# 100-weight_average.py
-def weight_average(own_lists=[]):
-    if not own_lists:
-        return 0
-
-    avg_num = 0
-    size_den = 0
-
-    for tup in own_lists:
-        avg_num += tup[0] * tup[1]
-        size_den += tup[1]
-
-    return (avg_num / size_den)
+def square_matrix_map(mtx=[]):
+    return (list(map(lambda sk: list(map(lambda kh: kh ** 2, sk[:])), mtx)))
 
