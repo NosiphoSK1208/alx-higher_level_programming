d2f5315326a77f2af2e71267df44231133da4c7f
NosiphoSK1208 <Simphiwe95.k@gmail.com>
2023-05-30 19:51:34 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py
+""" defines a class Square"""
+
+
+class Square:
+    """ Square class"""
+    def __init__(self, size):
+        """ size Initialize class """
+        self.__size = size
